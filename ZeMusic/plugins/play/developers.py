2043101d0a7470c63f93db8dd9d11bccbe75cd67
afrotoo66 <131810077+afrotoo66@users.noreply.github.com>
2024-02-03 16:00:51 +0200

    Add files via upload

---
+++ ZeMusic/plugins/play/developers.py
@@ -0,0 +1,41 @@
+import asyncio
+
+import os
+import time
+import requests
+from config import START_IMG_URL
+from pyrogram import filters
+import random
+from pyrogram import Client
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup
+from strings.filters import command
+from ZeMusic import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
+from ZeMusic import app
+from random import  choice, randint
+
+#          
+                
+@app.on_message(filters.command(["عفرتو","المبرمج عفرتو","مبرمج السورس","مبرمج"],"")
+)
+async def huhh(client: Client, message: Message):
+    await message.reply_photo(
+        photo=f"https://telegra.ph/file/96857cb597b588139fdd5.jpg",
+        caption=f"""◉ 𝙽𝙰𝙼𝙴 : ❪[ْ𓆩⧛ َ 𝘼َِ𝙁َِ𝙍َِ𝙊َِ𝙊َِ𝙏َِ𝙊ِ ┇ عـ๋͜‏ـۂفــ͡ـࢪتوُ ⧚𓆪](https://t.me/IIUll_l)❫
+◉ 𝚄𝚂𝙴𝚁 : ❪ @IIUll_l ❫
+◉ 𝙸𝙳      : ❪ `5904216848` ❫
+◉ 𝙱𝙸𝙾    : ❪ for me (@UI_VM)  ❫""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "ْ𓆩⧛ َ 𝘼َِ𝙁َِ𝙍َِ𝙊َِ𝙊َِ𝙏َِ𝙊ِ ┇ عـ๋͜‏ـۂفــ͡ـࢪتوُ ⧚𓆪", url=f"https://t.me/IIUll_l"), 
+                 ],[
+                   InlineKeyboardButton(
+                        "「𝚂𝙾𝚞𝚁𝚂 𝙰𝙵𝚁𝙾𝚃𝙾𝙾」", url=f"https://t.me/UI_VM"),
+                ],
+
+            ]
+
+        ),
+
+    )
---
+++ ZeMusic/plugins/play/السورس.py
@@ -0,0 +1,45 @@
+import asyncio
+
+import os
+import time
+import requests
+from pyrogram import filters
+import random
+from pyrogram import Client
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup
+from strings.filters import command
+from ZeMusic import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
+from ZeMusic import app
+from random import  choice, randint
+
+                
+@app.on_message(
+    command(["سورس","‹ السورس ›","عفرتو","السورس", "سورس عفرتو"])
+)
+async def huhh(client: Client, message: Message):
+    await message.reply_photo(
+        photo=f"https://telegra.ph/file/04b2f1f1c808dc49db35b.jpg",
+        caption=f"""**ᯓ 𝚆𝙴𝙻𝙲𝙾𝙼𝙴 𝚃𝙾 𝚂𝙾𝚞𝚁𝚂 𝙰𝙵𝚁𝙾𝚃𝙾𝙾](https://t.me/UI_VM)**""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "المطور", url=f"https://t.me/IIUll_l"), 
+                    
+                
+                    InlineKeyboardButton(
+                        "‹ الدعم ›", url=f"https://t.me/T_Y_E_X"),
+                ],[
+                    
+                
+                    InlineKeyboardButton(
+                        "‹ السورس ›", url=f"https://t.me/UI_VM"),
+                
+        ],
+
+            ]
+
+        ),
+
+    )
+
