3449cbda587f716b4dc61bab6a7a1efd1169a191
afrotoo66 <131810077+afrotoo66@users.noreply.github.com>
2024-02-09 00:52:55 +0200

    Update start & end video chat.py

--- ZeMusic/plugins/play/start & end video chat.py
+++ ZeMusic/plugins/play/start & end video chat.py
@@ -7,7 +7,7 @@ from pyrogram import Client, filters
 from strings import get_command
 from strings.filters import command
 from pyrogram.types import *
-from YukkiMusic import app
+from ZeMusic import app
 
 #▒▒▒▒▒▒▒▒▒▇▇▇▇▇▇▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▇▇▇▇▇▇▇▇▇▇▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
 #▒▒▒▒▒▒▒▇▇▒▒▒▒▒▒▇▇▒▒▒▒▒▒▒▒▒▒▒▒▒ ▒▒▇▇▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
