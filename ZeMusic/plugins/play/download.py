4e3dcf1993defeae16ae774465c59a24c293db9c
afrotoo66 <131810077+afrotoo66@users.noreply.github.com>
2024-02-14 22:42:39 +0200

    Update download.py

--- ZeMusic/plugins/play/download.py
+++ ZeMusic/plugins/play/download.py
@@ -1,35 +1,28 @@
 import os
-import re
-import time
-import asyncio
-import traceback
-
-import lyricsgenius
-
-import aiofiles
-import aiohttp
 import requests
-import wget
-import yt_dlp
-from pyrogram import Client, filters
-from pyrogram.errors import FloodWait, MessageNotModified
-from pyrogram.types import Message
-from youtube_search import YoutubeSearch
-from youtubesearchpython import VideosSearch
-from yt_dlp import YoutubeDL
-from ZeMusic import app 
 
-from ZeMusic.plugins.play.filters import command, other_filters
+import aiohttp
+import aiofiles
+
+import yt_dlp
+from yt_dlp import YoutubeDL
+from pyrogram import Client, filters
+from pyrogram.errors import FloodWait
+from pyrogram.types import Message, InputTextMessageContent
+from youtube_search import YoutubeSearch
+
+from ZeMusic import app
+from ZeMusic.plugins.play.filters import command
 
 def remove_if_exists(path):
     if os.path.exists(path):
         os.remove(path)
 
 
-@Client.on_message(command(["/song", "نزل", "اغاني", "موسيقي", "تنزيل", "/music"]) & ~filters.edited)
-async def song_downloader(_, message):
+@app.on_message(command(["/song", "بحث", "/music"]))
+async def song_downloader(client, message: Message):
     query = " ".join(message.command[1:])
-    m = await message.reply("🎬 العثور علي الاغنيه \n√")
+    m = await message.reply_text("<b>⇜ جـارِ البحث عـن المقطـع الصـوتـي . . .</b>")
     ydl_ops = {
         'format': 'bestaudio[ext=m4a]',
         'keepvideo': True,
@@ -49,29 +42,28 @@ async def song_downloader(_, message):
         duration = results[0]["duration"]
 
     except Exception as e:
-        await m.edit("🥹 عايز تنزل اي\n√")
+        await m.edit("- لم يتم العثـور على نتائج ؟!\n- حـاول مجـدداً . . .")
         print(str(e))
         return
-    await m.edit("🥹 عايز تنزل اي\n√")
+    await m.edit("<b>⇜ جـارِ التحميل ▬▭ . . .</b>")
     try:
         with yt_dlp.YoutubeDL(ydl_ops) as ydl:
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = f"🎬 υρᥣ᥆ᥲ️ժᥱᖇ @Q_XUQ "
+        rep = f"𖡃 ᴅᴏᴡɴʟᴏᴀᴅᴇᴅ ʙʏ @{app.username} "
         host = str(info_dict["uploader"])
         secmul, dur, dur_arr = 1, 0, duration.split(":")
         for i in range(len(dur_arr) - 1, -1, -1):
             dur += int(float(dur_arr[i])) * secmul
             secmul *= 60
-        await m.edit("🥹 تحميل الاغنيه\n√")
+        await m.edit("<b>⇜ جـارِ الرفـع ▬▬ . . .</b>")
         await message.reply_audio(
-            audio_file,
+            audio=audio_file,
             caption=rep,
+            title=title,
             performer=host,
             thumb=thumb_name,
-            parse_mode="md",
-            title=title,
             duration=dur,
         )
         await m.delete()
@@ -79,59 +71,9 @@ async def song_downloader(_, message):
     except Exception as e:
         await m.edit(" error, wait for bot owner to fix")
         print(e)
+
     try:
         remove_if_exists(audio_file)
         remove_if_exists(thumb_name)
     except Exception as e:
         print(e)
-
-
-@Client.on_message(
-    command(["/vsong", "/video", "حمل", "يوتيوب", "فيديو", "تحميل"]) & ~filters.edited
-)
-async def video_downloader(_, message):
-    ydl_opts = {
-        "format": "best",
-        "keepvideo": True,
-        "prefer_ffmpeg": False,
-        "geo_bypass": True,
-        "outtmpl": "%(title)s.%(ext)s",
-        "quite": True,
-    }
-    query = " ".join(message.command[1:])
-    try:
-        results = YoutubeSearch(query, max_results=1).to_dict()
-        link = f"https://youtube.com{results[0]['url_suffix']}"
-        title = results[0]["title"][:40]
-        thumbnail = results[0]["thumbnails"][0]
-        thumb_name = f"{title}.jpg"
-        thumb = requests.get(thumbnail, allow_redirects=True)
-        open(thumb_name, "wb").write(thumb.content)
-        results[0]["duration"]
-        results[0]["url_suffix"]
-        results[0]["views"]
-        message.from_user.mention
-    except Exception as e:
-        print(e)
-    try:
-        msg = await message.reply("🎥 تحميل الفيديو\n√")
-        with YoutubeDL(ydl_opts) as ytdl:
-            ytdl_data = ytdl.extract_info(link, download=True)
-            file_name = ytdl.prepare_filename(ytdl_data)
-    except Exception as e:
-        traceback.print_exc()
-        return await msg.edit(f"🥹 عايز تنزل اي\n√")
-    preview = wget.download(thumbnail)
-    await msg.edit("🎥 تحميل فيديو\n√...")
-    await message.reply_video(
-        file_name,
-        duration=int(ytdl_data["duration"]),
-        thumb=preview,
-        caption=ytdl_data["title"],
-    )
-    try:
-        remove_if_exists(file_name)
-        await msg.delete()
-    except Exception as e:
-        print(e)
-          
