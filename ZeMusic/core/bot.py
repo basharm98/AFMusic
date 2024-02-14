ba5587252c2b6efd18ded6c8a75ea3fdbbda21ac
afrotoo66 <131810077+afrotoo66@users.noreply.github.com>
2024-02-15 00:21:18 +0200

    Update bot.py

--- ZeMusic/core/bot.py
+++ ZeMusic/core/bot.py
@@ -45,7 +45,7 @@ class Mody(Client):
         a = await self.get_chat_member(config.LOGGER_ID, self.id)
         if a.status != ChatMemberStatus.ADMINISTRATOR:
             LOGGER(__name__).error(
-                "Please promote your bot as an admin in your log group/channel."
+                " قم برفـع البـوت مشـرفـاً بكافة الصلاحيات في مجموعـة السجـل ."
             )
             exit()
         LOGGER(__name__).info(f"Music Bot Started as {self.name}")
