c5b692ffd599f5039942887650b486cf94ee7a68
afrotoo66 <131810077+afrotoo66@users.noreply.github.com>
2024-02-14 23:50:18 +0200

    Update autoend.py

--- ZeMusic/plugins/sudo/autoend.py
+++ ZeMusic/plugins/sudo/autoend.py
@@ -6,19 +6,19 @@ from ZeMusic.misc import SUDOERS
 from ZeMusic.utils.database import autoend_off, autoend_on
 
 
-@app.on_message(filters.command("autoend") & SUDOERS)
+@app.on_message(filters.command("المغادرة التلقائية") & SUDOERS)
 async def auto_end_stream(_, message: Message):
-    usage = "<b>ᴇxᴀᴍᴘʟᴇ :</b>\n\n/autoend [ᴇɴᴀʙʟᴇ | ᴅɪsᴀʙʟᴇ]"
+    usage = "<b>ᴇxᴀᴍᴘʟᴇ :</b>\n\n/المغادرة التلقائية [تفعيل | تعطيل]"
     if len(message.command) != 2:
         return await message.reply_text(usage)
     state = message.text.split(None, 1)[1].strip().lower()
-    if state == "enable":
+    if state == "تفعيل":
         await autoend_on()
         await message.reply_text(
-            "» ᴀᴜᴛᴏ ᴇɴᴅ sᴛʀᴇᴀᴍ ᴇɴᴀʙʟᴇᴅ.\n\nᴀssɪsᴛᴀɴᴛ ᴡɪʟʟ ᴀᴜᴛᴏᴍᴀᴛɪᴄᴀʟʟʏ ʟᴇᴀᴠᴇ ᴛʜᴇ ᴠɪᴅᴇᴏᴄʜᴀᴛ ᴀғᴛᴇʀ ғᴇᴡ ᴍɪɴs ᴡʜᴇɴ ɴᴏ ᴏɴᴇ ɪs ʟɪsᴛᴇɴɪɴɢ."
+            "» تم تفعيل المغادرة التلقائية للمساعد .. بنجاح\n\nالحساب المساعد سوف يقوم بمغادرة الدردشة تلقائياً .. عند عدم وجود اشخاص في المكالمة."
         )
-    elif state == "disable":
+    elif state == "تعطيل":
         await autoend_off()
-        await message.reply_text("» ᴀᴜᴛᴏ ᴇɴᴅ sᴛʀᴇᴀᴍ ᴅɪsᴀʙʟᴇᴅ.")
+        await message.reply_text("» تم تعطيل وضع المغادرة التلقائية للمساعد .. بنجاح.")
     else:
         await message.reply_text(usage)
