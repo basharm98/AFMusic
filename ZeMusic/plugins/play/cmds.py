e021d7d52c6da99679e171daca0c2bf945e5c812
abdallah981 <62890648+abdallah981@users.noreply.github.com>
2024-02-20 20:54:39 +0300

    cmds.py

--- ZeMusic/plugins/play/cmds.py
+++ ZeMusic/plugins/play/cmds.py
@@ -16,7 +16,7 @@ async def zdatsr(client: Client, message: Message):
     name = usr.first_name
     usrnam = usr.username
     await message.reply_photo(
-        photo=f"https://telegra.ph/file/04b2f1f1c808dc49db35b.jpg",
+        photo=f"https://telegra.ph/file/1a77a02bdb06d55051845.jpg",
         caption=f"""<b>» مرحبـاً بك عـزيـزي </b> {message.from_user.mention} .\n\n<b>» استخـدم الازرار بالاسفـل 𝄞\n» لـ تصفـح اوامـر الميـوزك 🥁</b>""",
         reply_markup=InlineKeyboardMarkup(
             [
@@ -35,7 +35,7 @@ async def zdatsr(client: Client, message: Message):
                     InlineKeyboardButton(name, url=f"https://t.me/{usrnam}"),
                 ],[
                     InlineKeyboardButton(
-                        "•✯ 「𝙰𝙵𝚁𝙾𝚃𝙾𝙾 𝙼𝚞𝚂𝙸𝙲」 ✯•", url="https://t.me/UI_VM"),
+                        "•✯ 『 𝙺𝙸𝙽𝙶 𝙼𝚄𝚂𝙸𝙲 』 ✯•", url="https://t.me/EF_19"),
                 ],
             ]
         ),
