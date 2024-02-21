358e54099460bcff47409d0234898922707f4f93
abdallah981 <62890648+abdallah981@users.noreply.github.com>
2024-02-21 19:13:49 +0300

    developers.py

--- ZeMusic/plugins/play/developers.py
+++ ZeMusic/plugins/play/developers.py
@@ -18,11 +18,14 @@ from random import  choice, randint
 @app.on_message(filters.command(["مطور","مطور السورس","مبرمج السورس","المطور"],"")
 )
 async def huhh(client: Client, message: Message):
+    usr = await client.get_users(5145609515)
+    name = usr.first_name
+    l_name = usr.last_name
+    usrnam = usr.username
+    bio = usr.bio
     await message.reply_photo(
         photo=f"https://telegra.ph/file/1a77a02bdb06d55051845.jpg",
-        caption=f"""**◉ 𝙽𝙰𝙼𝙴 : ❪ 🇾🇪⃤𝐀𝐁𝐃𝐔𝐋𝐋𝐀𝐇 个 ١9 ❫**
-**◉ 𝙸𝙳   : ❪ `5145609515` ❫**
-**◉ 𝚄𝚂𝙴𝚁 : ❪ @IC_19 ❫**""",
+        caption=f"""<b>◉ 𝙽𝙰𝙼𝙴 : ❪ {message.from_user.mention} ❫.\n◉ 𝙸𝙳   : ❪ `5145609515` ❫.\n◉ 𝙱𝙸𝙾  : ❪ {bio} ❫.</b>""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
