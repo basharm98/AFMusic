a10218d76821da542628ba4d1bc52b38299f0f19
abdallah981 <62890648+abdallah981@users.noreply.github.com>
2024-02-24 01:15:57 +0300

    Update pause.py

--- ZeMusic/plugins/admins/pause.py
+++ ZeMusic/plugins/admins/pause.py
@@ -11,11 +11,11 @@ from config import BANNED_USERS
 
 @app.on_message(filters.command(["pause", "cpause","ايقاف مؤقت","إيقاف مؤقت","وقف"]) & filters.group & ~BANNED_USERS)
 @AdminRightsCheck
-async def pause_admin(cli, message: Message, _, chat_id):
+async def pause_admin(cli, message: Message, chat_id):
     if not await is_music_playing(chat_id):
-        return await message.reply_text(_["admin_1"])
+        return await message.reply_text("لا يوجد مقطع موسيقي قيد التشغيل حالياً.")
     await music_off(chat_id)
     await Mody.pause_stream(chat_id)
     await message.reply_text(
-        _["admin_2"].format(message.from_user.mention), reply_markup=close_markup(_)
+        f"تم إيقاف الموسيقى مؤقتاً، {message.from_user.mention}.", reply_markup=close_markup()
     )
