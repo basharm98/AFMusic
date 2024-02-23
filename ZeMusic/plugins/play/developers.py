eaebc079c9e75899b552f1c4d5d0151cdfcfa4c6
abdallah981 <62890648+abdallah981@users.noreply.github.com>
2024-02-23 20:26:27 +0300

    Update developers.py

--- ZeMusic/plugins/play/developers.py
+++ ZeMusic/plugins/play/developers.py
@@ -1,45 +1,40 @@
+import asyncio
 
-from telebot.types import *
-from telebot.handler_backends import ContinueHandling
+import os
+import time
+import requests
+from config import START_IMG_URL
+from pyrogram import filters
 import random
-
+from pyrogram import Client, filters, emoji
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup
+from strings.filters import command
+from ZeMusic import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
+from ZeMusic import app
+from random import  choice, randint
 
 #          
                 
-def Identification_(message: Message):
-    user_ = message.from_user
-    msg_text = message.text
-    if (
-        msg_text in ["المطور", "مطور"]
-    ):
-        
-        def Mrk():
-            mrk = InlineKeyboardMarkup()
-            btn = InlineKeyboardButton(
-                text=chan_inf.title if chan_inf.title else chan_inf.first_name,
-                url="https://t.me/" + chan_inf.username,
-            )
-            mrk.add(btn)
-            return mrk
+@app.on_message(filters.command(["مطور","مطور السورس","مبرمج السورس","المطور"],"")
+)
+async def huhh(client: Client, message: Message):
+    await message.reply_photo(
+        photo=f"https://telegra.ph/file/1a77a02bdb06d55051845.jpg",
+        caption=f"""<b>◉ 𝙽𝙰𝙼𝙴 : ❪ 🇾🇪⃤𝐀𝐁𝐃𝐔𝐋𝐋𝐀𝐇 个 ١9 ❫</b>
+<b>◉ 𝙸𝙳   : ❪ 5145609515 ❫</b>
+<b>◉ 𝚄𝚂𝙴𝚁 : ❪ @IC_19 ❫</b>""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "『 🇾🇪⃤𝐀𝐁𝐃𝐔𝐋𝐋𝐀𝐇 个 ١9 』", url=f"https://t.me/IC_19"), 
+                 ],[
+                   InlineKeyboardButton(
+                        "『 𝙺𝙸𝙽𝙶 𝚂𝙾𝚄𝚁𝙲𝙴 』", url=f"https://t.me/EF_19"),
+                ],
 
-        # استخدم المعرف "5145609515" لجلب معلومات المستخدم
-        bio = bot.get_chat(5145609515)
-        Photo_user = f"https://t.me/{bio.username}"
-        ttxt = f"""✯︙𝙽𝙰𝙼𝙴 : {bio.first_name}
-✯︙𝚄𝚂𝙴𝚁𝙽𝙰𝙼𝙴 : @{bio.username}
-✯︙𝙸𝙳 : {bio.id} .
-✯︙𝙱𝙸𝙾 :  {bio.bio}) ."""
-        try:
-            bot.send_photo(
-                Photo_user,
-                reply_to_message_id=message.id,
-                caption=ttxt,
-                reply_markup=Mrk(),
-            )
-        except:
-            bot.send_message(
-                ttxt,
-                reply_to_message_id=message.id,
-                reply_markup=Get_prerson(name=bio.first_name, id=bio.id),
-            )
+            ]
 
+        ),
+
+    )
