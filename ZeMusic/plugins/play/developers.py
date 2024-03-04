2f9c6cfb233d8a489713b50bea0bb7a51842bbda
Abdullah <62890648+king-1-9@users.noreply.github.com>
2024-03-04 20:29:03 +0300

    Update developers.py

--- ZeMusic/plugins/play/developers.py
+++ ZeMusic/plugins/play/developers.py
@@ -13,24 +13,16 @@ from ZeMusic import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
 from ZeMusic import app
 from random import  choice, randint
 
-from pyrogram.types import ChatMemberUpdated
 #          
                 
 @app.on_message(filters.command(["مطور","مطور السورس","مبرمج السورس","المطور"],"")
 )
 async def huhh(client: Client, message: Message):
-    dev_id = 5145609515
-    info = await app.get_chat(dev_id)
-    name = info.first_name
-    bio = info.bio
-
-    await app.download_media(info.photo.big_file_id, file_name=os.path.join("downloads", "developer.jpg"))     
     await message.reply_photo(
-        photo="downloads/developer.jpg",
-        caption=f"<b>◉ 𝙽𝙰𝙼𝙴 : ❪ 🇾🇪⃤𝐀𝐁𝐃𝐔𝐋𝐋𝐀𝐇 个 ١9 ❫</b>
+        photo=f"https://telegra.ph/file/1a77a02bdb06d55051845.jpg",
+        caption=f"""<b>◉ 𝙽𝙰𝙼𝙴 : ❪ 🇾🇪⃤𝐀𝐁𝐃𝐔𝐋𝐋𝐀𝐇 个 ١9 ❫</b>
 <b>◉ 𝙸𝙳   : ❪ 5145609515 ❫</b>
-<b>◉ 𝚄𝚂𝙴𝚁 : ❪ @IC_19 ❫</b>
-◉ 𝙱𝙸𝙾  : {bio}",
+<b>◉ 𝚄𝚂𝙴𝚁 : ❪ @IC_19 ❫</b>""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
