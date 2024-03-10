3e913bca96a8a2661cc99fab934a260b16bcafbb
Abdullah <62890648+king-1-9@users.noreply.github.com>
2024-03-10 12:17:54 +0300

    Update bot.py

--- ZeMusic/plugins/models/bot.py
+++ ZeMusic/plugins/models/bot.py
@@ -1,11 +1,10 @@
-#✘ ITALY MUSIC @I6ALY ✘
 import asyncio
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram import filters, Client
 from ZeMusic import app
 from config import OWNER_ID
 
-@app.on_message(filters.command(['بوت'], prefixes=""))
+@app.on_message(filters.command(["بوت"], prefixes=""))
 async def ZeMusic(client: Client, message: Message):
     me = await client.get_me()
     bot_username = me.username
@@ -21,9 +20,9 @@ async def ZeMusic(client: Client, message: Message):
              rank = "** مالك الس‍‌ورس 🫶🏻 **"
         elif user_id == OWNER_ID:
              rank = "مـالك الـبوت العظمه 🫡"
-        elif member.status == 'creator':
+        elif member.status == "creator":
              rank = "**مـالك الـبـار 🫡.**"
-        elif member.status == 'administrator':
+        elif member.status == "administrator":
              rank = "**مـشـرف الـبـار🫡**"
         else:
              rank = "**لاسف انت عضو فقير🥺💔**"
@@ -31,7 +30,6 @@ async def ZeMusic(client: Client, message: Message):
         print(e)
         rank = "مش عرفنلو مله ده😒"
     async for photo in client.iter_profile_photos("me", limit=1):
-                    await message.reply_photo(photo.file_id,       caption=f"""**نعم حبيبي :** {italy} 🥰❤\n**انا اسمي القميل :** {bot_name} 🥺🙈\n**رتبتك هي :** {rank}""", reply_markup=keyboard)
+                    await message.reply_photo(photo.file_id, caption=f"""**نعم حبيبي :** {italy} 🥰❤\n**انا اسمي القميل :** {bot_name} 🥺🙈\n**رتبتك هي :** {rank}""", reply_markup=keyboard)
 
 #✘ ITALY MUSIC @I6ALY ✘
-
