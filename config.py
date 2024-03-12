05b4a3798aa28b3e34e5c103b104d051eeb25b90
Abdullah <62890648+king-1-9@users.noreply.github.com>
2024-03-12 19:38:32 +0300

    Update config.py

--- config.py
+++ config.py
@@ -77,7 +77,7 @@ autoclean = []
 confirmer = {}
 
 
-START_IMG_URL = "https://graph.org/file/2f199ef466e7950dd5e79.jpg"
+START_IMG_URL = getenv("START_IMG_URL")
 PING_IMG_URL = "https://te.legra.ph/file/b8a0c1a00db3e57522b53.jpg"
 PLAYLIST_IMG_URL = "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
 STATS_IMG_URL = "https://te.legra.ph/file/e906c2def5afe8a9b9120.jpg"
