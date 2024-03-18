170a7e64873233ea92b2464c4058aa1885d16633
Abdullah <62890648+king-1-9@users.noreply.github.com>
2024-03-18 23:17:59 +0300

    Update سجل الجروبات.py

--- ZeMusic/plugins/play/سجل الجروبات.py
+++ ZeMusic/plugins/play/سجل الجروبات.py
@@ -8,8 +8,8 @@ from pyrogram import Client, filters
 from pyrogram.types import Message
 from ZeMusic import app
 from ZeMusic.utils.database import get_served_chats
-from config import LOGGER_ID
 
+LOGGER_ID = -1001756578463
 
 async def lul_message(chat_id: int, message: str):
     await app.send_message(chat_id=chat_id, text=message)
