2752c701975071929ac7ce99ef94401b4bc9b531
Abdullah <62890648+king-1-9@users.noreply.github.com>
2024-03-18 03:11:45 +0300

    Update المالك.py

--- ZeMusic/plugins/play/المالك.py
+++ ZeMusic/plugins/play/المالك.py
@@ -15,25 +15,23 @@ from random import choice, randint
 @app.on_message(
   command(["مطور","المطور"])
 )
-async def devid(client: Client, message: Message):
+async def huhh(client: Client, message: Message):
     us_id = OWNER_ID  # استخدام قيمة OWNER_ID من ملف config.py
 
     # احصل على معلومات المطور باستخدام الايدي المحدد
-    developer_info = await client.get_users(us_id)
+    developer_info = await client.get_users([us_id])  # قم بتمرير الايدي كقائمة واحدة
 
     await message.reply_photo(
-        photo=developer_info.photo.big_file_id,
-        caption=f"""<b>⌯ المطور :</b> <a href="tg://user?id={us_id}">{developer_info.first_name}</a>
+        photo=developer_info[0].photo.big_file_id,  # الحصول على الصورة من العنصر الأول في القائمة
+        caption=f"""<b>⌯ المطور :</b> <a href="tg://user?id={us_id}">{developer_info[0].first_name}</a>
         
-<b>⌯ معرف المطور :</b> @{developer_info.username}""",
+<b>⌯ معرف المطور :</b> @{developer_info[0].username}""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "المطور", url=f"https://t.me/{developer_info.username}"), 
+                        "المطور", url=f"https://t.me/{developer_info[0].username}"), 
                  ],
             ]
-
         ),
-
     )
