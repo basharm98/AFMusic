0bc8358b5c0e99e2c18092d71b1bc5a6ef090478
Abdullah <62890648+king-1-9@users.noreply.github.com>
2024-03-18 04:33:13 +0300

    Update المالك.py

--- ZeMusic/plugins/play/المالك.py
+++ ZeMusic/plugins/play/المالك.py
@@ -1,23 +1,17 @@
 import asyncio
 import os
-import time
-import requests
-from config import START_IMG_URL, OWNER_ID
 from pyrogram import filters
-import random
-from pyrogram import Client, filters, emoji
-from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup
+from pyrogram import Client, emoji
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from strings.filters import command
-from ZeMusic import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
 from ZeMusic import app
-from random import choice, randint
+from config import OWNER_ID
 
 @app.on_message(
-  command(["مطور","المطور"])
+    command(["مطور","المطور"])
 )
 async def devid(client: Client, message: Message):
-    dev_id = OWNER_ID 
-    info = await app.get_chat(dev_id)
+    info = await client.get_users(OWNER_ID)
     name = info.first_name
     bio = info.bio
     
@@ -27,11 +21,11 @@ async def devid(client: Client, message: Message):
         caption=f"""<b>⌯ المطور :</b> <a href="tg://user?id={dev_id}">{name}</a>
         
 <b>⌯ البايو :</b> {bio}""",
-        reply_markup = InlineKeyboardMarkup(
+        reply_markup=InlineKeyboardMarkup(
             [
                 [ 
                     InlineKeyboardButton(name, user_id=dev_id)
                 ]
             ]
         ), 
-      )
+    )
