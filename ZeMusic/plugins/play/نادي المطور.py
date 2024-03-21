3dc205f7a8adf0e8ce1b31dd13137c4f91830ef7
Abdullah <62890648+king-1-9@users.noreply.github.com>
2024-03-21 17:40:11 +0300

    Update نادي المطور.py

--- ZeMusic/plugins/play/نادي المطور.py
+++ ZeMusic/plugins/play/نادي المطور.py
@@ -16,32 +16,32 @@ from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from config import OWNER_ID
 
-muntazer = "EF_19"
+muntazer = "IC_19"
 
 @app.on_message(filters.command("نادي المطور", [".", ""]) & filters.group)
 async def call_dev(client: Client, message: Message):
     chat = message.chat.id
     gti = message.chat.title
+    
     link = await app.export_chat_invite_link(chat)
     usr = await client.get_users(message.from_user.id)
-    chatusername = f"@{message.chat.username}"
     user_id = message.from_user.id
     user_ab = message.from_user.username
     user_name = message.from_user.first_name
     buttons = [[InlineKeyboardButton(gti, url=f"{link}")]]
     reply_markup = InlineKeyboardMarkup(buttons)
     
-    await app.send_message(OWNER_ID, f"- قام {message.from_user.mention}\n"
-                                     f"- بمناداتك عزيزي المطور\n"
-                                     f"- الأيدي {user_id}\n"
-                                     f"- اليوزر @{user_ab}\n"
-                                     f"- ايدي المجموعة {message.chat.id}\n"
-                                     f"- الرابط {chatusername}",
+    await app.send_message(OWNER_ID, f"<b>- قام {message.from_user.mention}\n</b>"
+                                     f"<b>- بمناداتك عزيزي المطور\n</b>"
+                                     f"<b>- الأيدي {user_id}\n</b>"
+                                     f"<b>- اليوزر @{user_ab}\n</b>"
+                                     f"<b>- ايدي المجموعة {message.chat.id}\n</b>"
+                                     f"<b>- الرابط {link}</b>",
                                      reply_markup=reply_markup)
 
     # إنشاء زر "اونلاين"
-    online_button = InlineKeyboardButton("< 𝙺𝙸𝙽𝙶 >", url=f"https://t.me/{muntazer}")
+    online_button = InlineKeyboardButton("『 🇾🇪⃤𝐀𝐁𝐃𝐔𝐋𝐋𝐀𝐇 个 ١9 』", url=f"https://t.me/{muntazer}")
     
-    await message.reply_text(f"~ **تم إرسال النداء إلى مطور البوت\n\n-› Master -› @{muntazer} .",
+    await message.reply_text(f"<b> ⌯ تم إرسال النداء إلى مطور البوت\n\n ⌯ Dᥱꪜ -› @{muntazer} .</b>",
                              disable_web_page_preview=True,
                              reply_markup=InlineKeyboardMarkup([[online_button]]))
