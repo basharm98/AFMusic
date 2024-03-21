50eb86e1540a5ab3b5d625e7900fc2f078713768
Abdullah <62890648+king-1-9@users.noreply.github.com>
2024-03-22 02:05:56 +0300

    Update قناة الاشتراك.py

--- ZeMusic/plugins/play/قناة الاشتراك.py
+++ ZeMusic/plugins/play/قناة الاشتراك.py
@@ -1,15 +1,15 @@
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from AarohiX import app
+from ZeMusic import app
 from config import Muntazer, OWNER_ID
 
 @app.on_message(filters.private & filters.user(OWNER_ID))
 async def must_join_channel(_, message):
     if "‹ قناة الاشتراك ›" in message.text:
-        link = f"https://t.me/{Muntazer}"
+        link = f"https://t.me/EF_19"
         await message.reply(
-            text=f"~ عزيزي المطور \n~ هذا هي قناة الاشتراك الاجباري @{Muntazer} .",
+            text=f"~ عزيزي المطور \n~ هذا هي قناة الاشتراك الاجباري @EF_19 .",
             reply_markup=InlineKeyboardMarkup([
-                [InlineKeyboardButton("~ freedom .", url=link)]
+                [InlineKeyboardButton("~ 𝙺𝙸𝙽𝙶 .", url=link)]
             ])
         )
