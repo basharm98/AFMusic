7c74c57aa9cb46f3c6bcdd85695e7b0e803920f2
Abdullah <62890648+king-1-9@users.noreply.github.com>
2024-03-21 17:46:48 +0300

    Update نادي المطور.py

--- ZeMusic/plugins/play/نادي المطور.py
+++ ZeMusic/plugins/play/نادي المطور.py
@@ -22,7 +22,7 @@ muntazer = "IC_19"
 async def call_dev(client: Client, message: Message):
     chat = message.chat.id
     gti = message.chat.title
-    
+    chatusername = f"@{message.chat.username}"
     link = await app.export_chat_invite_link(chat)
     usr = await client.get_users(message.from_user.id)
     user_id = message.from_user.id
@@ -31,12 +31,12 @@ async def call_dev(client: Client, message: Message):
     buttons = [[InlineKeyboardButton(gti, url=f"{link}")]]
     reply_markup = InlineKeyboardMarkup(buttons)
     
-    await app.send_message(OWNER_ID, f"<b>- قام {message.from_user.mention}\n</b>"
-                                     f"<b>- بمناداتك عزيزي المطور\n</b>"
-                                     f"<b>- الأيدي {user_id}\n</b>"
-                                     f"<b>- اليوزر @{user_ab}\n</b>"
-                                     f"<b>- ايدي المجموعة {message.chat.id}\n</b>"
-                                     f"<b>- الرابط {link}</b>",
+    await app.send_message(OWNER_ID, f"<b>⌯ قام {message.from_user.mention}\n</b>"
+                                     f"<b>⌯ بمناداتك عزيزي المطور\n</b>"
+                                     f"<b>⌯ الأيدي {user_id}\n</b>"
+                                     f"<b>⌯ اليوزر @{user_ab}\n</b>"
+                                     f"<b>⌯ ايدي المجموعة {message.chat.id}\n</b>"
+                                     f"<b>⌯ يوزر المجموعه {chatusername}</b>",
                                      reply_markup=reply_markup)
 
     # إنشاء زر "اونلاين"
