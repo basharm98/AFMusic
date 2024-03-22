b9c2bc15fb07ea29d30df3eaa0c6b9a2f14ac439
Abdullah <62890648+king-1-9@users.noreply.github.com>
2024-03-23 01:30:33 +0300

    Update Telegraph.py

--- ZeMusic/plugins/models/Telegraph.py
+++ ZeMusic/plugins/models/Telegraph.py
@@ -3,7 +3,7 @@ from typing import Optional
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message, CallbackQuery
 from telegraph import upload_file
-from AarohiX import app
+from ZeMusic import app
 
 
 #---------------FUNCTION---------------#
