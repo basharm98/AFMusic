1f74507a2def87f177165f40df8a1dce23475c6f
Abdullah <62890648+king-1-9@users.noreply.github.com>
2024-03-23 03:38:32 +0300

    Update قناة الاشتراك.py

--- ZeMusic/plugins/models/قناة الاشتراك.py
+++ ZeMusic/plugins/models/قناة الاشتراك.py
@@ -1,4 +1,4 @@
-from pyrogram import Client, filters
+"""from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from ZeMusic import app
 from config import OWNER_ID
