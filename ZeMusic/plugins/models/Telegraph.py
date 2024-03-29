b6826b3f96259b24ac896700acd6d60bad2669bf
Abdullah <62890648+king-1-9@users.noreply.github.com>
2024-03-30 02:54:15 +0300

    Update Telegraph.py

--- ZeMusic/plugins/models/Telegraph.py
+++ ZeMusic/plugins/models/Telegraph.py
@@ -21,17 +21,17 @@ def get_file_id(msg: Message) -> Optional[Message]:
 #---------------FUNCTION---------------#
 
 
-@app.on_message(filters.command("tgm"))
+@app.on_message(filters.command("تلغراف", "ميديا", "تلكراف", "تلجراف"))
 async def telegraph_upload(bot, update):
     replied = update.reply_to_message
     if not replied:
-        return await update.reply_text("Rᴇᴘʟʏ Tᴏ A Pʜᴏᴛᴏ Oʀ Vɪᴅᴇᴏ Uɴᴅᴇʀ 5ᴍʙ")
+        return await update.reply_text("⌯ ¦ قم بالرد على ملف وسائط مدعوم.\n⌯ ¦ حط صوره او فيديو و اكتب عليها.")
     file_info = get_file_id(replied)
     if not file_info:
-        return await update.reply_text("Not Supported!")
-    text = await update.reply_text(text="<code>Downloading To My Server ...</code>", disable_web_page_preview=True)   
+        return await update.reply_text("⌯ ¦ ياغبي غير مدعوم.\n⌯ ¦ حط صوره و اكتب عليها.")
+    text = await update.reply_text(text="<code>انتظر يتم التحميل ...</code>", disable_web_page_preview=True)   
     media = await update.reply_to_message.download()   
-    await text.edit_text(text="<code>Downloading Completed. Now I am Uploading to telegra.ph Link ...</code>", disable_web_page_preview=True)                                            
+    await text.edit_text(text="<code>اكتمل التحميل. الآن يتم رفعه إلى التلغراف ...</code>", disable_web_page_preview=True)                                            
     try:
         response = upload_file(media)
     except Exception as error:
@@ -44,13 +44,13 @@ async def telegraph_upload(bot, update):
         print(error)
         return    
     await text.edit_text(
-        text=f"<b>ʜᴇʀᴇ ɪs ʏᴏᴜʀ ɢᴇɴᴇʀᴀᴛᴇᴅ ᴛᴇ.ʟᴇɢʀᴀ.ᴘʜ ʟɪɴᴋ 💞 :-</b>\n\n<code>https://te.legra.ph{response[0]}</code>",
+        text=f"<b>تم انشاء الرابط 💞 :-</b>\n\n<code>https://te.legra.ph{response[0]}</code>",
         disable_web_page_preview=True,
         reply_markup=InlineKeyboardMarkup( [[
-            InlineKeyboardButton(text="ᴏᴘᴇɴ ʟɪɴᴋ", url=f"https://te.legra.ph{response[0]}"),
-            InlineKeyboardButton(text="sʜᴀʀᴇ ʟɪɴᴋ", url=f"https://telegram.me/share/url?url=https://te.legra.ph{response[0]}")
+            InlineKeyboardButton(text="🎯 ¦ افـتح الـرابـط", url=f"https://te.legra.ph{response[0]}"),
+            InlineKeyboardButton(text="♻️ ¦ مشـاركه الـرابـط", url=f"https://telegram.me/share/url?url=https://te.legra.ph{response[0]}")
             ],[
-            InlineKeyboardButton(text="✗ ᴄʟᴏsᴇ ✗", callback_data="close")
+            InlineKeyboardButton(text="✘ اغلاق ✘", callback_data="close")
             ]])
         )
     
