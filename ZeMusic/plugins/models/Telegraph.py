26dfc906d8498d92e7bb39279a1c841fa02259b9
Abdullah <62890648+king-1-9@users.noreply.github.com>
2024-03-30 04:09:03 +0300

    Update Telegraph.py

--- ZeMusic/plugins/models/Telegraph.py
+++ ZeMusic/plugins/models/Telegraph.py
@@ -44,8 +44,8 @@ async def telegraph_upload(bot, update):
         print(error)
         return    
     await text.edit_text(
-        text=f"<b>⎉╎الــرابـط : </b><a href='https://graph.org{response[0]}'>اضغــط هنـــا</a>\   \n<b>⎉╎مشاركة : </b><a href='https://telegram.me/share/url?url=https://te.legra.ph{response[0]}'>اضغــط هنـــا</a>",
-        disable_web_page_preview=True,
+        text=f"""<b>⎉╎الــرابـط : </b><a href="https://graph.org{response[0]}">اضغــط هنـــا</a>\n<b>⎉╎مشاركة : </b><a href="https://telegram.me/share/url?url=https://graph.org{response[0]}">اضغــط هنـــا</a>""",
+        link_preview=True,
         reply_markup=InlineKeyboardMarkup( [[
             InlineKeyboardButton(text="✘ اغلاق ✘", callback_data="close")
             ]])
