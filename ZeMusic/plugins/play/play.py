ecad81bbda2acdd99a7f9948a7c8476afda14c66
Abdullah <62890648+king-1-9@users.noreply.github.com>
2024-04-20 13:24:33 +0300

    Update play.py

--- ZeMusic/plugins/play/play.py
+++ ZeMusic/plugins/play/play.py
@@ -32,7 +32,7 @@ from config import BANNED_USERS, lyrical
             "تشغيل",
             "شغل",
             "vplay",
-            "vفيديو",
+            "فيديو",
             "cplay",
             "cvplay",
             "playforce",
@@ -327,8 +327,8 @@ async def play_commnd(
             )
         slider = True
         query = message.text.split(None, 1)[1]
-        if "-v" in query:
-            query = query.replace("-v", "")
+        if "-ف" in query:
+            query = query.replace("-ف", "")
         try:
             details, track_id = await YouTube.track(query)
         except:
@@ -347,7 +347,7 @@ async def play_commnd(
                     _,
                     track_id,
                     user_id,
-                    "v" if video else "a",
+                    "ف" if video else "a",
                     "c" if channel else "g",
                     "f" if fplay else "d",
                 )
@@ -481,7 +481,7 @@ async def play_music(client, CallbackQuery, _):
             _["play_13"],
             reply_markup=InlineKeyboardMarkup(buttons),
         )
-    video = True if mode == "v" else None
+    video = True if mode == "ف" else None
     ffplay = True if fplay == "f" else None
     try:
         await stream(
@@ -546,7 +546,7 @@ async def play_playlists_command(client, CallbackQuery, _):
         _["play_2"].format(channel) if channel else _["play_1"]
     )
     videoid = lyrical.get(videoid)
-    video = True if mode == "v" else None
+    video = True if mode == "ف" else None
     ffplay = True if fplay == "f" else None
     spotify = True
     if ptype == "yt":
