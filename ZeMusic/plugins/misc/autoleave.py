12ee5de63a74c5903a34118424ba92455364ea76
Abdullah <62890648+king-1-9@users.noreply.github.com>
2024-04-22 17:29:28 +0300

    Update autoleave.py

--- ZeMusic/plugins/misc/autoleave.py
+++ ZeMusic/plugins/misc/autoleave.py
@@ -10,30 +10,37 @@ from ZeMusic.utils.database import get_client, is_active_chat, is_autoend
 
 
 async def auto_leave():
-    if config.AUTO_LEAVING_ASSISTANT:
-        while not await asyncio.sleep(300):
+    if config.AUTO_LEAVING_ASSISTANT == str(True):
+        while not await asyncio.sleep(
+            config.AUTO_LEAVE_ASSISTANT_TIME
+        ):
             from ZeMusic.core.userbot import assistants
 
             for num in assistants:
                 client = await get_client(num)
                 left = 0
                 try:
-                    async for i in client.get_dialogs():
-                        if i.chat.type in [
-                            ChatType.SUPERGROUP,
-                            ChatType.GROUP,
-                            ChatType.CHANNEL,
+                    async for i in client.iter_dialogs():
+                        chat_type = i.chat.type
+                        if chat_type in [
+                            "supergroup",
+                            "group",
+                            "channel",
                         ]:
+                            chat_id = i.chat.id
                             if (
-                                i.chat.id != config.LOGGER_ID
-                                and i.chat.id != -1001565463580
-                                and i.chat.id != -1001565463580
+                                chat_id != config.LOG_GROUP_ID
+                                and chat_id != -1001190342892
+                                and chat_id != -1001733534088
+                                and chat_id != -1001443281821
                             ):
                                 if left == 20:
                                     continue
-                                if not await is_active_chat(i.chat.id):
+                                if not await is_active_chat(chat_id):
                                     try:
-                                        await client.leave_chat(i.chat.id)
+                                        await client.leave_chat(
+                                            chat_id
+                                        )
                                         left += 1
                                     except:
                                         continue
