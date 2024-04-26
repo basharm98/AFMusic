6f6204ebabbe201d4dec582d399867376c9ecb86
Abdullah <62890648+king-1-9@users.noreply.github.com>
2024-04-26 20:48:32 +0300

    Update bot.py

--- ZeMusic/plugins/models/bot.py
+++ ZeMusic/plugins/models/bot.py
@@ -9,19 +9,6 @@ async def ZeMusic(client: Client, message: Message):
     italy = message.from_user.mention 
     user_id = message.from_user.id
     chat_id = message.chat.id
-    try:
-        member = await client.get_chat_member(chat_id, user_id)
-        if user_id == 5145609515:
-             rank = "<a href='tg://user?id={user_id}'>مطور السورس</a>"
-        elif user_id == OWNER_ID:
-             rank = "<a href='tg://user?id={user_id}'>المطور</a>"
-        else:
-             rank = italy
-    except Exception as e:
-        print(e)
-        rank = ""
-
-    #await message.reply_text(f"<b>⌯ نعم حبيبي :</b> {italy}\n<b>⌯ انا اسمي القميل :</b> {bot_name} 🥺🙈 {rank}", reply_markup=keyboard)
     await message.reply_text(f"<b>⌯ مرحباً عزيزي :</b> {rank}\n<b>⌯ ما هي الاغنيه التي تريد تشغيلها أو البحث عنها</b>")
 
 
