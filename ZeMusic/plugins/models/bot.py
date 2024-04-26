3267078536b0d02801b121f75bceefe0f30c9b3d
Abdullah <62890648+king-1-9@users.noreply.github.com>
2024-04-26 20:37:06 +0300

    Update bot.py

--- ZeMusic/plugins/models/bot.py
+++ ZeMusic/plugins/models/bot.py
@@ -1,4 +1,3 @@
-"""
 import asyncio
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram import filters, Client
@@ -7,25 +6,22 @@ from config import OWNER_ID
 
 @app.on_message(filters.command("بوت", prefixes=""))
 async def ZeMusic(client: Client, message: Message):
-    me = await client.get_me()
-    bot_username = me.username
-    bot_name = me.first_name
-    italy = message.from_user.mention
-    button = InlineKeyboardButton("اضف البوت الى مجموعتك🏅", url=f"https://t.me/{bot_username}?startgroup=true")
-    keyboard = InlineKeyboardMarkup([[button]])
+    italy = message.from_user.mention 
     user_id = message.from_user.id
     chat_id = message.chat.id
     try:
         member = await client.get_chat_member(chat_id, user_id)
         if user_id == 5145609515:
-             rank = "\n<b>⌯ رتبتك هي : مطور الس‍‌ورس 🫶🏻.</b>"
+             rank = "<a href='tg://user?id={user_id}'>مطور السورس</a>"
         elif user_id == OWNER_ID:
-             rank = "\n<b>⌯ رتبتك هي : مـالك الـبوت 🫡.</b>"
+             rank = "<a href='tg://user?id={user_id}'>المطور</a>"
         else:
-             rank = ""
+             rank = italy
     except Exception as e:
         print(e)
         rank = ""
 
-    await message.reply_text(f"<b>⌯ نعم حبيبي :</b> {italy}\n<b>⌯ انا اسمي القميل :</b> {bot_name} 🥺🙈 {rank}", reply_markup=keyboard)
-"""
+    #await message.reply_text(f"<b>⌯ نعم حبيبي :</b> {italy}\n<b>⌯ انا اسمي القميل :</b> {bot_name} 🥺🙈 {rank}", reply_markup=keyboard)
+    await message.reply_text(f"<b>⌯ مرحباً عزيزي :</b> {rank}\n<b>⌯ ما هي الاغنيه التي تريد تشغيلها أو البحث عنها</b>")
+
+
